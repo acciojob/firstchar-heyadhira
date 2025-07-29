362453cb6843ea7d7cf30de44d174770432da95d
Adhira <hey.adhira@gmail.com>
2025-07-29 10:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
   // your code here
+	
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
-alert(firstChar(text));
+alert(firstChar(text[0]));
